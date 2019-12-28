27af22ad593e0e9d6905ff433cff2e8e8f2286bc
Paul Spenke <paul@spenke.de>
2019-12-28 21:53:08 +0100

    First Steps setup router buefy

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from 'buefy'
+import Recipes from './data/recipes'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
 new Vue({
+  data () {
+    return {
+      recipes: Recipes
+    }
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="recipe in $root.recipes" :key="recipe.uuid">
+        <router-link :to="'/'+recipe.slug">
+          {{recipe.name}}
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
