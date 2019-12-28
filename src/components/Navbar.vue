1684ad4ac7b22bd5e3c6cf4096d84747fae384c2
Paul Spenke <paul@spenke.de>
2019-12-28 21:55:29 +0100

    added files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="../assets/chef-hat.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        >
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Timer</strong>
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/data/recipes.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    uuid: '41e0fac9-76e4-47e8-ab27-77f7d4613f2c',
+    slug: 'paulsEierkuchen',
+    name: 'Pauls Eierkuchen',
+    categories: [
+      'Frühstück',
+      'Pfanne'
+    ],
+    utilities: [
+      'Pfanne',
+      'Schüssel'
+    ],
+    ingredients: [
+      {
+        amount: 3,
+        unit: [' Ei', ' Eier']
+      },
+      {
+        amount: 125,
+        unit: 'ml Milch'
+      },
+      {
+        amount: 125,
+        unit: 'g Mehl'
+      }
+    ],
+    steps: [
+      'Schritt 1',
+      'Schritt 2'
+    ]
+  }
+]
